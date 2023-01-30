9b8072eb129f64e5c52dd17e2706e3b5d670b0a8
Rodorigo S. Cavalcanti <rspievakc@users.noreply.github.com>
2023-01-30 09:27:53 -0300

    Initial setup. Playing with multi-module gradle.

---
+++ module-2/src/main/java/rspievakc/GreetingResource.java
@@ -0,0 +1,35 @@
+package rspievakc;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Path("/123")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Book hello() {
+        return Book.builder()
+                .name("Hello World")
+                .description("Test 123")
+                .build();
+    }
+
+    @Data
+    @Builder(toBuilder=true)
+    @AllArgsConstructor
+    public static class Book {
+
+        private String name;
+        private String description;
+
+    }
+}
+
